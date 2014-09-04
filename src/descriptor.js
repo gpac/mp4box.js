174582d09bdcbf6d2ee7259d0731a39fd0613ada
cconcolato <cyril.concolato@telecom-paristech.fr>
2014-09-04 08:54:07 +0200

    align license headers with LICENSE file

--- src/descriptor.js
+++ src/descriptor.js
@@ -1,17 +1,6 @@
-/*
+/* 
  * Copyright (c) 2012-2013. Telecom ParisTech/TSI/MM/GPAC Cyril Concolato
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * This notice must stay in all subsequent versions of this code.
+ * License: BSD-3-Clause (see LICENSE file)
  */
 var MPEG4DescriptorParser = function () {
 	var ES_DescrTag 			= 0x03;
