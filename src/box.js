80fd3262e1eaa14bb0669cc363ab4a4bf2547049
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-02-18 11:48:18 +0100

    fix regression in parsing of 3GPP Timed Text Sample Entry

--- src/box.js
+++ src/box.js
@@ -408,6 +408,17 @@ BoxParser.stppBox.prototype.parse = function(stream) {
 	this.parseFooter(stream);
 }
 
+BoxParser.tx3gBox.prototype.parse = function(stream) {
+	this.parseHeader(stream);
+	this.displayFlags = stream.readUint32();
+	this.horizontal_justification = stream.readInt8();
+	this.vertical_justification = stream.readInt8();
+	this.bg_color_rgba = stream.readUint8Array(4);
+	this.box_record = stream.readInt16Array(4);
+	this.style_record = stream.readUint8Array(12);
+	this.parseFooter(stream);
+}
+
 BoxParser.ftypBox.prototype.parse = function(stream) {
 	this.major_brand = stream.readString(4);
 	this.minor_version = stream.readUint32();
