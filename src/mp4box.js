2342eb608c10d56a6617214a53312de0c1da1beb
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-03-11 09:51:57 +0100

    fix seeking when all samples are RAP

--- src/mp4box.js
+++ src/mp4box.js
@@ -595,11 +595,6 @@ MP4Box.prototype.seekTrack = function(time, useRap, trak) {
 	var timescale;
 	for (j = 0; j < trak.samples.length; j++) {
 		sample = trak.samples[j];
-		if (useRap && sample.is_rap) {
-			rap_offset = sample.offset;
-			rap_time = sample.cts;
-			rap_seek_sample_num = j;
-		}
 		if (j === 0) {
 			seek_offset = sample.offset;
 			seek_sample_num = 0;
@@ -608,6 +603,11 @@ MP4Box.prototype.seekTrack = function(time, useRap, trak) {
 			seek_offset = trak.samples[j-1].offset;
 			seek_sample_num = j-1;
 			break;
+		} 
+		if (useRap && sample.is_rap) {
+			rap_offset = sample.offset;
+			rap_time = sample.cts;
+			rap_seek_sample_num = j;
 		}
 	}
 	if (useRap) {
