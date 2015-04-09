f2d479c13ce57389b55e6c51cfe211589d8ed8c5
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-04-09 19:30:03 +0200

    fix for when MPEG4Descriptor is missing

--- src/box-parse.js
+++ src/box-parse.js
@@ -331,10 +331,9 @@ BoxParser.esdsBox.prototype.parse = function(stream) {
 	this.parseFullHeader(stream);
 	this.data = stream.readUint8Array(this.size);
 	this.size = 0;
-	if (MPEG4DescriptorParser) {
+	if (typeof MPEG4DescriptorParser !== "undefined") {
 		var esd_parser = new MPEG4DescriptorParser();
 		this.esd = esd_parser.parseOneDescriptor(new DataStream(this.data.buffer, 0, DataStream.BIG_ENDIAN));
-		delete this.data;
 	} 
 }
 
