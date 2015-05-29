1107aee455f1e79f9916443611f62f6260a531fd
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-05-29 16:50:41 +0200

    add elst writing

--- src/box-write.js
+++ src/box-write.js
@@ -562,3 +562,19 @@ BoxParser.sidxBox.prototype.write = function(stream) {
 		stream.writeUint32(ref.starts_with_SAP << 31 | ref.SAP_type << 28 | ref.SAP_delta_time);
 	}
 }
+
+BoxParser.elstBox.prototype.write = function(stream) {
+	this.version = 0;	
+	this.flags = 0;
+	this.size = 4+12*this.entries.length;
+	this.writeHeader(stream);
+	stream.writeUint32(this.entries.length);
+	for (var i = 0; i < this.entries.length; i++) {
+		var entry = this.entries[i];
+		stream.writeUint32(entry.segment_duration);
+		stream.writeInt32(entry.media_time);
+		stream.writeInt16(entry.media_rate_integer);
+		stream.writeInt16(entry.media_rate_fraction);
+	}
+}
+
