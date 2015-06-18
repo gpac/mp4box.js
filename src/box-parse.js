3823f9fec98db60c410e410f2e781578d96a530a
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-06-18 16:15:48 +0200

    fix Log message

--- src/box-parse.js
+++ src/box-parse.js
@@ -785,7 +785,7 @@ BoxParser["url Box"].prototype.parse = function(stream) {
 	if (this.flags !== 0x000001) {
 		this.location = stream.readCString();
 	} else if (this.size - this.hdr_size) {
-		Log.warn("BoxParser", "Invalid urlBox - contains data but flag not set");
+		Log.w("BoxParser", "Invalid urlBox - contains data but flag not set");
 		this.location = stream.readString(this.size - this.hdr_size);
 	}
 }
