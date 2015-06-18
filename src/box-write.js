461e54840c14a0a2a4a4bfc144a44fdcd3c04f6a
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-06-18 11:36:14 +0200

    fix misalignement with sidx spec field names (3a6c6e0)

--- src/box-write.js
+++ src/box-write.js
@@ -557,7 +557,7 @@ BoxParser.sidxBox.prototype.write = function(stream) {
 	stream.writeUint16(this.references.length);
 	for (var i = 0; i < this.references.length; i++) {
 		var ref = this.references[i];
-		stream.writeUint32(ref.reference_type << 31 | ref.reference_size);
+		stream.writeUint32(ref.reference_type << 31 | ref.referenced_size);
 		stream.writeUint32(ref.subsegment_duration);
 		stream.writeUint32(ref.starts_with_SAP << 31 | ref.SAP_type << 28 | ref.SAP_delta_time);
 	}
