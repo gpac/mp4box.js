c5f3819241c0310c5806c201576d9ebb1e4fb76c
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-07-10 16:40:57 +0200

    small change in node test

--- test/node/info.js
+++ test/node/info.js
@@ -1,7 +1,8 @@
+var fs = require('fs');
+var mp4boxModule = require('mp4box');
 
 if (process.argv.length > 2) {
-	var mp4box = new (require('mp4box').MP4Box)();
-	var fs = require('fs');
+	var mp4box = new mp4boxModule.MP4Box();
 	var arrayBuffer = new Uint8Array(fs.readFileSync(process.argv[2])).buffer;
 	arrayBuffer.fileStart = 0;
 
