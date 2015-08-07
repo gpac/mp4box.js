958ec59d7d126a1bf88a822ff32114d980911e12
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-08-07 19:00:37 +0200

    fix wrong encoding of url box

--- src/writing/url.js
+++ src/writing/url.js
@@ -1,10 +1,10 @@
 BoxParser["url Box"].prototype.write = function(stream) {
 	this.version = 0;	
 	if (this.location) {
-		this.flags = 0x000001;
+		this.flags = 0;
 		this.size = this.location.length+1;
 	} else {
-		this.flags = 0;
+		this.flags = 0x000001;
 		this.size = 0;
 	}
 	this.writeHeader(stream);
