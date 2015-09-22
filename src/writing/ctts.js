46a676c30a48fb7d0ff44a1dca34a51ba59ab4a4
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-09-22 17:26:11 +0200

    use version 0 of ctts for Firefox

--- src/writing/ctts.js
+++ src/writing/ctts.js
@@ -1,13 +1,17 @@
 BoxParser.cttsBox.prototype.write = function(stream) {
 	var i;
-	this.version = 1;
+	this.version = 0;
 	this.flags = 0;
 	this.size = 4+8*this.sample_counts.length;
 	this.writeHeader(stream);
 	stream.writeUint32(this.sample_counts.length);
 	for(i=0; i<this.sample_counts.length; i++) {
 		stream.writeUint32(this.sample_counts[i]);
-		stream.writeInt32(this.sample_offsets[i]); /* signed */
+		if (this.version === 1) {
+			stream.writeInt32(this.sample_offsets[i]); /* signed */
+		} else {			
+			stream.writeUInt32(this.sample_offsets[i]); /* unsigned */
+		}
 	}
 }
 
