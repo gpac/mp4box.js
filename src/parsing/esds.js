8ca3d6d69ae014602a1f95082f5254343117ae25
cconcolato <cyril.concolato@telecom-paristech.fr>
2016-02-10 20:56:32 +0100

    avoid overwrite of box buffer for descriptors

--- src/parsing/esds.js
+++ src/parsing/esds.js
@@ -1,9 +1,9 @@
 BoxParser.esdsBox.prototype.parse = function(stream) {
 	this.parseFullHeader(stream);
-	this.data = stream.readUint8Array(this.size-this.hdr_size);
+	var esd_data = stream.readUint8Array(this.size-this.hdr_size);
 	if (typeof MPEG4DescriptorParser !== "undefined") {
 		var esd_parser = new MPEG4DescriptorParser();
-		this.esd = esd_parser.parseOneDescriptor(new DataStream(this.data.buffer, 0, DataStream.BIG_ENDIAN));
+		this.esd = esd_parser.parseOneDescriptor(new DataStream(esd_data.buffer, 0, DataStream.BIG_ENDIAN));
 	} 
 }
 
