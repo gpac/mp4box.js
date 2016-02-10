9e77ae431d1da05de458dc52586a95d88886a170
cconcolato <cyril.concolato@telecom-paristech.fr>
2016-02-10 20:57:09 +0100

    fix wrong writing of stsz

--- src/writing/stsz.js
+++ src/writing/stsz.js
@@ -2,7 +2,7 @@ BoxParser.stszBox.prototype.write = function(stream) {
 	var i;
 	this.version = 0;
 	this.flags = 0;
-	this.size = 8+12*this.sample_sizes.length;
+	this.size = 8+4*this.sample_sizes.length;
 	this.writeHeader(stream);
 	stream.writeUint32(0);
 	stream.writeUint32(this.sample_sizes.length);
