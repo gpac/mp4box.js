9aa3247185716799af65e77620f2fb92bb964d55
cconcolato <cyril.concolato@telecom-paristech.fr>
2016-02-11 19:15:09 +0100

    compact writing of stsz to match MP4Box

--- src/writing/stsz.js
+++ src/writing/stsz.js
@@ -1,11 +1,34 @@
 BoxParser.stszBox.prototype.write = function(stream) {
 	var i;
+	var constant = true;
 	this.version = 0;
 	this.flags = 0;
-	this.size = 8+4*this.sample_sizes.length;
+	if (this.sample_sizes.length > 0) {
+		i = 0;
+		while (i+1 < this.sample_sizes.length) {
+			if (this.sample_sizes[i+1] !==  this.sample_sizes[0]) {
+				constant = false;
+				break;
+			} else {
+				i++;
+			}
+		}
+	} else {
+		constant = false;
+	}
+	this.size = 8;
+	if (!constant) {
+		this.size += 4*this.sample_sizes.length;
+	}
 	this.writeHeader(stream);
-	stream.writeUint32(0);
+	if (!constant) {
+		stream.writeUint32(0);
+	} else {
+		stream.writeUint32(this.sample_sizes[0]);
+	}
 	stream.writeUint32(this.sample_sizes.length);
-	stream.writeUint32Array(this.sample_sizes);
+	if (!constant) {
+		stream.writeUint32Array(this.sample_sizes);
+	}	
 }
 
