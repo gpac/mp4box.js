e023eec465fee4590ded8d78fdbb5ce205093cee
cconcolato <cyril.concolato@telecom-paristech.fr>
2016-03-14 17:07:05 +0100

    add missing sample entries stxt and tx3g after reorg

---
+++ src/parsing/sampleentries/stxt.js
@@ -0,0 +1,7 @@
+BoxParser.stxtSampleEntry.prototype.parse = function(stream) {
+	this.parseHeader(stream);
+	this.content_encoding = stream.readCString();
+	this.mime_format = stream.readCString();
+	this.parseFooter(stream);
+}
+
