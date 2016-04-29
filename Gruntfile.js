b624d43ed32b1d86b86bfa95a5a00b187d9acdbf
cconcolato <cyril.concolato@telecom-paristech.fr>
2016-04-29 13:50:37 +0200

    trying to fix travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,9 +79,9 @@ module.exports = function(grunt) {
         'test/**/*.js', 
         // Exclude the following from lint 
         '!test/lib/**/*.js', 
-        '!test/mp4/**/*.js',
-        '!test/trackviewers/**/*.js', 
-        '!test/coverage/**/*.js', 
+        '!test/mp4/**/*.js'//,
+        //'!test/trackviewers/**/*.js', 
+        //'!test/coverage/**/*.js', 
       ],
       options: {
         // options here to override JSHint defaults
