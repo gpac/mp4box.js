719e8b630af9400fe76d092379caa9c3f35c1798
Tim Branyen <tim@tabdeveloper.com>
2019-04-02 10:46:10 -0700

    Revert "change parsing of SPS/PPS in avcC"
    This reverts commit 1dc26b8040c2a9fff01999636c58e896d14a03f7.

--- src/parsing/avcC.js
+++ src/parsing/avcC.js
@@ -1,28 +1,28 @@
 BoxParser.createBoxCtor("avcC", function(stream) {
 	var i;
+	var nb_nalus;
+	var length;
 	var toparse;
 	this.configurationVersion = stream.readUint8();
 	this.AVCProfileIndication = stream.readUint8();
 	this.profile_compatibility = stream.readUint8();
 	this.AVCLevelIndication = stream.readUint8();
 	this.lengthSizeMinusOne = (stream.readUint8() & 0x3);
-	this.nb_SPS_nalus = (stream.readUint8() & 0x1F);
+	nb_nalus = (stream.readUint8() & 0x1F);
 	toparse = this.size - this.hdr_size - 6;
-	this.SPS = [];
-	for (i = 0; i < this.nb_SPS_nalus; i++) {
-		this.SPS[i] = {};
-		this.SPS[i].length = stream.readUint16();
-		this.SPS[i].nalu = stream.readUint8Array(this.SPS[i].length);
-		toparse -= 2+this.SPS[i].length;
+	this.SPS = new Array(nb_nalus);
+	for (i = 0; i < nb_nalus; i++) {
+		length = stream.readUint16();
+		this.SPS[i] = stream.readUint8Array(length);
+		toparse -= 2+length;
 	}
-	this.nb_PPS_nalus = stream.readUint8();
+	nb_nalus = stream.readUint8();
 	toparse--;
-	this.PPS = [];
-	for (i = 0; i < this.nb_PPS_nalus; i++) {
-		this.PPS[i] = {};
-		this.PPS[i].length = stream.readUint16();
-		this.PPS[i].nalu = stream.readUint8Array(this.PPS[i].length);
-		toparse -= 2+this.PPS[i].length;
+	this.PPS = new Array(nb_nalus);
+	for (i = 0; i < nb_nalus; i++) {
+		length = stream.readUint16();
+		this.PPS[i] = stream.readUint8Array(length);
+		toparse -= 2+length;
 	}
 	if (toparse>0) {
 		this.ext = stream.readUint8Array(toparse);
