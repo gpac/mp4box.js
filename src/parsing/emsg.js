184b73a71dd5d211f491073aa935b463e2b4637b
Cyril Concolato <cconcolato@netflix.com>
2020-11-04 11:15:24 -0800

    fix name of field in emsg v1 #213

--- src/parsing/emsg.js
+++ src/parsing/emsg.js
@@ -1,7 +1,7 @@
 BoxParser.createFullBoxCtor("emsg", function(stream) {
 	if (this.version == 1) {
 		this.timescale 					= stream.readUint32();
-		this.presentation_time_delta 	= stream.readUint64();
+		this.presentation_time 			= stream.readUint64();
 		this.event_duration			 	= stream.readUint32();
 		this.id 						= stream.readUint32();
 		this.scheme_id_uri 				= stream.readCString();
