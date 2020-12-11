37b79274bd2915683fc918c636070cde777627c7
Cyril Concolato <cconcolato@netflix.com>
2020-12-10 21:59:59 -0800

    avoid global variables

--- src/writing/tfdt.js
+++ src/writing/tfdt.js
@@ -1,6 +1,5 @@
-var UINT32_MAX = Math.pow(2, 32) - 1;
-
 BoxParser.tfdtBox.prototype.write = function(stream) {
+	var UINT32_MAX = Math.pow(2, 32) - 1;
 	// use version 1 if baseMediaDecodeTime does not fit 32 bits
 	this.version = this.baseMediaDecodeTime > UINT32_MAX ? 1 : 0;
 	this.flags = 0;
@@ -12,7 +11,7 @@ BoxParser.tfdtBox.prototype.write = function(stream) {
 	if (this.version === 1) {
 		stream.writeUint64(this.baseMediaDecodeTime);
 	} else {
-		stream.writeUint32(this.baseMediaDecodeTime); 
+		stream.writeUint32(this.baseMediaDecodeTime);
 	}
 }
 
