c8f9467083330da94aa0040c55baf54aca090938
Gabriel Hege <g+github@hege.cc>
2021-10-13 15:20:55 +0200

    vvcC field names closer to spec

--- src/parsing/vvcC.js
+++ src/parsing/vvcC.js
@@ -22,110 +22,99 @@ BoxParser.createFullBoxCtor("vvcC", function (stream) {
     }
   };
 
+  // VvcDecoderConfigurationRecord
   bitReader.stream_read_1_bytes(stream);
-  var reserved = bitReader.extract_bits(5);
-  // console.assert(reserved == 0x1f, "expect reserved bits to be all ones");
-  this.nal_unit_size = 1 + bitReader.extract_bits(2);
-  this.ptl_present = bitReader.extract_bits(1);
-  // console.assert(num_bits_remain === 0, "expect all bits to be consumed (" + num_bits_remain + ")");
+  bitReader.extract_bits(5);  // reserved
+  this.lengthSizeMinusOne = bitReader.extract_bits(2);
+  this.ptl_present_flag = bitReader.extract_bits(1);
 
-  if (this.ptl_present) {
+  if (this.ptl_present_flag) {
     bitReader.stream_read_2_bytes(stream);
     this.ols_idx = bitReader.extract_bits(9);
-    this.numTemporalLayers = bitReader.extract_bits(3);
-    this.constantFrameRate = bitReader.extract_bits(2);
-    this.chroma_format = bitReader.extract_bits(2);
-    // console.assert(num_bits_remain === 0, "expect all bits to be consumed (" + num_bits_remain + ")");
+    this.num_sublayers = bitReader.extract_bits(3);
+    this.constant_frame_rate = bitReader.extract_bits(2);
+    this.chroma_format_idc = bitReader.extract_bits(2);
 
     bitReader.stream_read_1_bytes(stream);
-    this.bit_depth = 8 + bitReader.extract_bits(3);
-    bitReader.extract_bits(5);
-    // console.assert(num_bits_remain === 0, "expect all bits to be consumed (" + num_bits_remain + ")");
+    this.bit_depth_minus8 = bitReader.extract_bits(3);
+    bitReader.extract_bits(5);  // reserved
 
-    //parse PTL
+    // VvcPTLRecord
     {
       bitReader.stream_read_2_bytes(stream);
-      bitReader.extract_bits(2);
-      this.num_constraint_info = bitReader.extract_bits(6);
+      bitReader.extract_bits(2);  // reserved
+      this.num_bytes_constraint_info = bitReader.extract_bits(6);
       this.general_profile_idc = bitReader.extract_bits(7);
       this.general_tier_flag = bitReader.extract_bits(1);
-      // console.assert(num_bits_remain === 0, "expect all bits to be consumed (" + num_bits_remain + ")");
 
       this.general_level_idc = stream.readUint8();
 
       bitReader.stream_read_1_bytes(stream);
-      this.ptl_frame_only_constraint = bitReader.extract_bits(1);
-      this.ptl_multilayer_enabled = bitReader.extract_bits(1);
-      // console.assert(num_bits_remain === 6, "expect 6 bits to remaining (" + num_bits_remain + ")");
+      this.ptl_frame_only_constraint_flag = bitReader.extract_bits(1);
+      this.ptl_multilayer_enabled_flag = bitReader.extract_bits(1);
 
-      this.general_constraint_info = new Uint8Array(this.num_constraint_info);
-      if (this.num_constraint_info) {
-        for (i = 0; i < this.num_constraint_info - 1; i++) {
+      this.general_constraint_info = new Uint8Array(this.num_bytes_constraint_info);
+      if (this.num_bytes_constraint_info) {
+        for (i = 0; i < this.num_bytes_constraint_info - 1; i++) {
           var cnstr1 = bitReader.extract_bits(6);
           bitReader.stream_read_1_bytes(stream);
           var cnstr2 = bitReader.extract_bits(2);
 
           this.general_constraint_info[i] = ((cnstr1 << 2) | cnstr2);
         }
-        this.general_constraint_info[this.num_constraint_info - 1] = bitReader.extract_bits(6);
+        this.general_constraint_info[this.num_bytes_constraint_info - 1] = bitReader.extract_bits(6);
       } else {
         //forbidden in spec!
         bitReader.extract_bits(6);
       }
-      // console.assert(num_bits_remain === 0, "expect all bits to be consumed (" + num_bits_remain + ")");
 
       bitReader.stream_read_1_bytes(stream);
       this.ptl_sublayer_present_mask = 0;
-      for (j = this.numTemporalLayers - 2; j >= 0; --j) {
+      for (j = this.num_sublayers - 2; j >= 0; --j) {
         var val = bitReader.extract_bits(1);
         this.ptl_sublayer_present_mask |= val << j;
       }
-      for (j = this.numTemporalLayers; j <= 8 && this.numTemporalLayers > 1; ++j) {
-        bitReader.extract_bits(1);
+      for (j = this.num_sublayers; j <= 8 && this.num_sublayers > 1; ++j) {
+        bitReader.extract_bits(1);  // ptl_reserved_zero_bit
       }
-      // console.assert(num_bits_remain === 0, "expect all bits to be consumed (" + num_bits_remain + ")");
 
-      for (j = this.numTemporalLayers - 2; j >= 0; --j) {
+      for (j = this.num_sublayers - 2; j >= 0; --j) {
         if (this.ptl_sublayer_present_mask & (1 << j)) {
           this.sublayer_level_idc[j] = stream.readUint8();
         }
       }
 
-      this.num_sub_profiles = stream.readUint8();
-      if (this.num_sub_profiles) {
-        this.sub_profiles_idc = [];
-        for (i = 0; i < this.num_sub_profiles; i++) {
-          this.sub_profiles_idc.push(stream.readUint32());
+      this.ptl_num_sub_profiles = stream.readUint8();
+      this.general_sub_profile_idc = [];
+      if (this.ptl_num_sub_profiles) {
+        for (i = 0; i < this.ptl_num_sub_profiles; i++) {
+          this.general_sub_profile_idc.push(stream.readUint32());
         }
       }
-    }
-    //end PTL
+    }  // end VvcPTLRecord
 
-    this.maxPictureWidth = stream.readUint16();
-    this.maxPictureHeight = stream.readUint16();
-    this.avgFrameRate = stream.readUint16();
+    this.max_picture_width = stream.readUint16();
+    this.max_picture_height = stream.readUint16();
+    this.avg_frame_rate = stream.readUint16();
   }
 
   var VVC_NALU_OPI = 12;
   var VVC_NALU_DEC_PARAM = 13;
 
-  this.param_arrays = [];
-  var numOfArrays = stream.readUint8();
-  for (i = 0; i < numOfArrays; i++) {
+  this.nalu_arrays = [];
+  var num_of_arrays = stream.readUint8();
+  for (i = 0; i < num_of_arrays; i++) {
     var nalu_array = [];
-    this.param_arrays.push(nalu_array);
+    this.nalu_arrays.push(nalu_array);
 
     bitReader.stream_read_1_bytes(stream);
     nalu_array.completeness = bitReader.extract_bits(1);
-    bitReader.extract_bits(2);
+    bitReader.extract_bits(2);  // reserved
     nalu_array.nalu_type = bitReader.extract_bits(5);
 
-    var numNalus;
+    var numNalus = 1;
     if (nalu_array.nalu_type != VVC_NALU_DEC_PARAM && nalu_array.nalu_type != VVC_NALU_OPI) {
       numNalus = stream.readUint16();
-    }
-    else {
-      numNalus = 1;
     }
 
     for (j = 0; j < numNalus; j++) {
