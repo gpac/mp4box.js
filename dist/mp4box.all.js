a8e8e2bff86d515d1c0c1da006e1d18543d3f0e1
hughfenghen <hughfenghen@gmail.com>
2023-06-12 19:09:27 +0800

    fix: esds box size is incorrect

--- dist/mp4box.all.js
+++ dist/mp4box.all.js
@@ -3256,6 +3256,7 @@ BoxParser.createEntityToGroupCtor("prgr");
 // file:src/parsing/esds.js
 BoxParser.createFullBoxCtor("esds", function(stream) {
 	var esd_data = stream.readUint8Array(this.size-this.hdr_size);
+	this.data = esd_data
 	if (typeof MPEG4DescriptorParser !== "undefined") {
 		var esd_parser = new MPEG4DescriptorParser();
 		this.esd = esd_parser.parseOneDescriptor(new DataStream(esd_data.buffer, 0, DataStream.BIG_ENDIAN));
--- src/parsing/esds.js
+++ src/parsing/esds.js
@@ -1,5 +1,6 @@
 BoxParser.createFullBoxCtor("esds", function(stream) {
 	var esd_data = stream.readUint8Array(this.size-this.hdr_size);
+	this.data = esd_data
 	if (typeof MPEG4DescriptorParser !== "undefined") {
 		var esd_parser = new MPEG4DescriptorParser();
 		this.esd = esd_parser.parseOneDescriptor(new DataStream(esd_data.buffer, 0, DataStream.BIG_ENDIAN));
