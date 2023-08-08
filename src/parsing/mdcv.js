797571ea64c91c87435772b5de7c14e3dff50ba0
Cyril Concolato <cconcolato@netflix.com>
2023-08-08 09:37:53 -0700

    improve display of mdcv content

--- src/parsing/mdcv.js
+++ src/parsing/mdcv.js
@@ -1,17 +1,18 @@
+function ColorPoint(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+ColorPoint.prototype.toString = function() {
+    return "("+this.x+","+this.y+")";
+}
+
 BoxParser.createBoxCtor("mdcv", function(stream) {
     this.display_primaries = [];
-    this.display_primaries[0] = {};
-    this.display_primaries[0].x = stream.readUint16();
-    this.display_primaries[0].y = stream.readUint16();
-    this.display_primaries[1] = {};
-    this.display_primaries[1].x = stream.readUint16();
-    this.display_primaries[1].y = stream.readUint16();
-    this.display_primaries[2] = {};
-    this.display_primaries[2].x = stream.readUint16();
-    this.display_primaries[2].y = stream.readUint16();
-    this.white_point = {};
-    this.white_point.x = stream.readUint16();
-    this.white_point.y = stream.readUint16();
+    this.display_primaries[0] = new ColorPoint(stream.readUint16(),stream.readUint16());
+    this.display_primaries[1] = new ColorPoint(stream.readUint16(),stream.readUint16());
+    this.display_primaries[2] = new ColorPoint(stream.readUint16(),stream.readUint16());
+    this.white_point = new ColorPoint(stream.readUint16(),stream.readUint16());
     this.max_display_mastering_luminance = stream.readUint32();
     this.min_display_mastering_luminance = stream.readUint32();
 });
