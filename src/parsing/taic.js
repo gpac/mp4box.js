e2a35016315dc27d5ba501bfa593fb3f061984b7
dukesook <devonsookhoo14@gmail.com>
2023-11-03 01:55:07 +0000

    If the values are unknown, display 'unknown'

--- src/parsing/taic.js
+++ src/parsing/taic.js
@@ -2,8 +2,13 @@ BoxParser.createFullBoxCtor("taic", function(stream) {
 	this.time_uncertainty = stream.readUint64();
 	this.correction_offset = stream.readInt64();
 	this.clock_drift_rate = stream.readFloat32();
-	//if (clock_drift_rate == 0x7FC0 0000)
-	//	value is an IEEE 754 quiet NaN.
-	//	Interpret the value as unknown 
 	this.clock_source = stream.readUint8();
+
+	if (this.time_uncertainty === 0xffffffffffffffff) {
+		this.time_uncertainty = "unknown";
+	}
+
+	if (this.correction_offset === 0x7fffffffffffffff) {
+		this.correction_offset = "unknown";
+	}
 });
