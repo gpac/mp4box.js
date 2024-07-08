b854e49297d0a08681ad42b0f9da8137a56e9be7
dukesook <devonsookhoo14@gmail.com>
2024-07-08 09:58:57 -0600

    bugfix - clock_type is the first two bits, not last two

--- src/parsing/taic.js
+++ src/parsing/taic.js
@@ -2,8 +2,9 @@ BoxParser.createFullBoxCtor("taic", function(stream) {
 	this.time_uncertainty = stream.readUint64();
 	this.clock_resolution = stream.readUint32();
 	this.clock_drift_rate = stream.readInt32();
-	this.clock_type = stream.readUint8();
-
+	var reserved_byte = stream.readUint8();
+	this.clock_type = (reserved_byte & 0xC0) >> 6;
+	
 	if (this.time_uncertainty === 0xffffffffffffffff) {
 		this.time_uncertainty = "unknown";
 	}
