gpac/mp4box.js

commit 92eb110a90c72cccafe5b19f52621bb181a56c3b

fix regression from #438

test/ui-helper.js
@@ -238,6 +238,14 @@ function createFancyTree(parent, fileobj) {
 			boxtreetable.html(generateBoxTable(node.data.box));
 		}
 	};
+	fancytree_options.renderTitle = function(event, data) {
+		var box = data.node.data.box;
+		var el = "<span class='fancytree-title'>";
+		el += box.type + '&nbsp;';
+		if (box.box_name)
+			el += "<span class='boxname'>(" + box.box_name + ")</span>";
+		return el;
+	};
 	boxtreediv.fancytree(fancytree_options);
 	fileobj.fancytree = boxtreediv.fancytree('getTree');
 }

test/boxfancytree.js
@@ -26,9 +26,6 @@ function getFancyTreeDataFromBoxes(boxes) {
 		var fancytree_node = {};
 		array.push(fancytree_node);
 		fancytree_node.title = box.type || i;
-		if (box.box_name) {
-			fancytree_node.title += ' <span class="boxname">(' + box.box_name + ')</span>';
-		}
 		fancytree_node.data = { 'box': box };
 		var child_prop_names = [ "boxes", "entries", "references", "subsamples",
 								 "items", "item_infos", "extents", "associations", "subsegments", "ranges", "seekLists", "seekPoints", "esd", "levels", "props"];

commit a6deb7c253aa07af4c0af22a8c1d4ae169274eac

Handle boxes with no type.
Show the box's index instead of "undefined" (regression from commit 92eb110).

test/ui-helper.js
@@ -241,7 +241,16 @@ function createFancyTree(parent, fileobj) {
 	fancytree_options.renderTitle = function(event, data) {
 		var box = data.node.data.box;
 		var el = "<span class='fancytree-title'>";
-		el += box.type + '&nbsp;';
+		var title = box.type;
+		if (!title) {
+			// Find index in parent.
+			var siblings = data.node.parent.children;
+			for (var idx = 0; idx < siblings.length; idx++) {
+				if (siblings[idx] == data.node) break;
+			}
+			title = idx;
+		}
+		el += title + '&nbsp;';
 		if (box.box_name)
 			el += "<span class='boxname'>(" + box.box_name + ")</span>";
 		el += "</span>";