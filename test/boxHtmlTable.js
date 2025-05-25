30b63b4c869864fdef8afb4a19ef76627c213b07
DenizUgur <DenizUgur@users.noreply.github.com>
2025-05-25 15:19:44 -0700

    minor fix

--- test/boxHtmlTable.js
+++ test/boxHtmlTable.js
@@ -38,7 +38,7 @@ function generateBoxTable(box, excluded_fields, additional_props, no_header) {
       continue;
     } else if (excluded_fields && excluded_fields.indexOf(prop) > -1) {
       continue;
-    } else if (box[prop] instanceof BoxParser.Box) {
+    } else if (box[prop] instanceof Box) {
       continue;
     } else if (typeof box[prop] === 'undefined') {
       continue;
