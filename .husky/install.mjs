07ce45347c4f4c1393a75273ab8dd075255b7af9
DenizUgur <DenizUgur@users.noreply.github.com>
2025-05-26 09:50:14 -0700

    chore: use husky recommendations

---
+++ .husky/install.mjs
@@ -0,0 +1,13 @@
+// Skip Husky install in production and CI
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+  process.exit(0);
+}
+
+// Install Husky hooks
+console.log('Installing Husky...');
+const husky = (await import('husky')).default;
+const output = husky();
+
+if (output && output.length > 0) {
+  console.log(output);
+}
