a864ffbf8680d5a3b0e3a50b54db957cb573d9cc
DenizUgur <DenizUgur@users.noreply.github.com>
2025-06-11 19:33:24 -0700

    test: finalize test with a roundtrip test

--- tests/conformance.test.ts
+++ tests/conformance.test.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import files from '../.cache/files.json';
-import { Box, createFile, Log, MP4BoxBuffer, type Movie } from '../entries/all';
+import { Box, createFile, Log, MP4BoxBuffer, MultiBufferStream, type Movie } from '../entries/all';
 import { BoxRegistry } from '../src/registry';
 
 const publishedPrefix = 'published/';
@@ -57,8 +57,8 @@ async function getFileRange(
   });
 }
 
-async function loadAndGetInfo(file_path: string, loadAll = false) {
-  const mp4 = createFile();
+async function loadAndGetInfo(file_path: string, loadAll = false, keepMdat = false) {
+  const mp4 = createFile(keepMdat);
   const ready = new Promise<Movie>(resolve => {
     mp4.onReady = resolve;
   });
@@ -93,6 +93,138 @@ function spyOnLog(notices: Set<string>, reportedUnknownBoxes: Set<string>) {
     warnSpy.mockRestore();
     errorSpy.mockRestore();
   };
+}
+
+interface Tree {
+  [key: string]: Tree | unknown;
+}
+type IncomingRoot = Record<string, string | object>;
+function traverseGPAC(root: IncomingRoot, reportedUnknownBoxes): Tree {
+  const inner = (_root: IncomingRoot, parent?: string): Tree => {
+    const result: Tree = {};
+
+    function addTypeToResult(item: Record<string, string | object>, inBox?: string) {
+      let type = item['@Type'];
+      if (type && typeof type === 'string') {
+        let query = type;
+        if (type === 'uuid' && '@UUID' in item && typeof item['@UUID'] === 'string')
+          query = item['@UUID'].replace(/-|\{|\}/g, '').toLowerCase();
+        if (!isKnownBoxType(query, inBox) && !reportedUnknownBoxes.has(query)) return;
+
+        const originalType = type;
+        if (type in result) {
+          const count = Object.keys(result).filter(key => key.startsWith(type as string)).length;
+          type = `${type}_${count}`;
+        }
+        result[type] = inner(item, originalType);
+      }
+    }
+
+    for (const [_, value] of Object.entries(_root)) {
+      if (Array.isArray(value)) {
+        for (const item of value) {
+          if (typeof item === 'object' && '@Type' in item) {
+            const container =
+              '@Type' in _root && typeof _root['@Type'] === 'string' && _root['@Type'];
+
+            addTypeToResult(item as IncomingRoot, container);
+          }
+        }
+      } else if (typeof value === 'object' && value !== null && '@Type' in value) {
+        addTypeToResult(value as IncomingRoot, parent);
+      }
+    }
+
+    return result;
+  };
+  return inner(root);
+}
+
+function traverseMP4(root: Array<Box>, testFile: string, withProperties = false): Tree {
+  const inner = (_root: Array<Box>): Tree => {
+    const result: Tree = {};
+
+    for (const box of _root) {
+      if (box.type) {
+        let type = box.type;
+        if (type in result) {
+          const count = Object.keys(result).filter(key => key.startsWith(type)).length;
+          type = `${type}_${count}`;
+        }
+
+        if (testFile.includes('sg-tl-st') && type === 'strk_1') {
+          // FIXME: FFC issue (MPEGGroup/FileFormatConformance#155)
+          continue;
+        }
+
+        if (testFile.includes('video_2500000bps_0') && type === 'mdat_1') {
+          continue; // FIXME: FFC issue (MPEGGroup/FileFormatConformance#131)
+        }
+
+        if (box.boxes?.length > 0) {
+          result[type] = inner(box.boxes);
+        } else if (box['entries']) {
+          result[type] = inner(box['entries']);
+        } else if (box['item_infos']) {
+          result[type] = inner(box['item_infos']);
+        } else if (box['references']) {
+          result[type] = inner(box['references']);
+        } else {
+          // FIXME: FFC issue, maxr isn't present in the gpac structure (MPEGGroup/FileFormatConformance#157)
+          if (testFile.includes('a2-foreman-QCIF-hinted') && box.type === 'maxr') continue;
+
+          // FIXME: FFC issue, gpac de-duplicates entries in sample entries
+          // This isn't a proper fix, but FFC doesn't change that much anyway
+          if (testFile.includes('hev1_clg1_header') && type.startsWith('colr_')) continue;
+
+          result[type] = {};
+        }
+
+        // Add properties if requested
+        if (withProperties) {
+          const ignoredKeys = [
+            'first_sample_index',
+            'sample_number',
+            'first_traf_merged',
+            'sample_groups_info',
+            'samples_duration',
+            'samples_size',
+            'sample_size',
+            'sample_sizes',
+            'used',
+            'samples',
+            'boxes',
+            'entries',
+            'item_infos',
+            'references',
+            'has_fragment_subsamples',
+            // FIXME: Remove the following keys
+            'start',
+            'size',
+          ];
+          const properties: Record<string, unknown> = {};
+          for (const [key, value] of Object.entries(box)) {
+            if (
+              !ignoredKeys.includes(key) &&
+              (typeof value === 'string' ||
+                typeof value === 'number' ||
+                typeof value === 'boolean' ||
+                (Array.isArray(value) &&
+                  value.every(v => typeof v === 'string' || typeof v === 'number')))
+            ) {
+              properties[key] = value;
+            }
+          }
+          if (Object.keys(properties).length > 0) {
+            result[type]['@'] = properties;
+          }
+        }
+      }
+    }
+
+    return result;
+  };
+  return inner(root);
 }
 
 Object.values(files.file_metadata).forEach(meta => {
@@ -150,104 +282,63 @@ describe('Conformance Tests', () => {
       expect(notices).toEqual(new Set());
       resetSpy();
 
-      interface Tree {
-        [key: string]: Tree | unknown;
-      }
-      type IncomingRoot = Record<string, string | object>;
-      const traverseGPAC = (root: IncomingRoot, parent?: string): Tree => {
-        const result: Tree = {};
-
-        function addTypeToResult(item: Record<string, string | object>, inBox?: string) {
-          let type = item['@Type'];
-          if (type && typeof type === 'string') {
-            let query = type;
-            if (type === 'uuid' && '@UUID' in item && typeof item['@UUID'] === 'string')
-              query = item['@UUID'].replace(/-|\{|\}/g, '').toLowerCase();
-            if (!isKnownBoxType(query, inBox) && !reportedUnknownBoxes.has(query)) return;
-
-            const originalType = type;
-            if (type in result) {
-              const count = Object.keys(result).filter(key =>
-                key.startsWith(type as string),
-              ).length;
-              type = `${type}_${count}`;
-            }
-            result[type] = traverseGPAC(item, originalType);
-          }
-        }
-
-        for (const [_, value] of Object.entries(root)) {
-          if (Array.isArray(value)) {
-            for (const item of value) {
-              if (typeof item === 'object' && '@Type' in item) {
-                const container =
-                  '@Type' in root && typeof root['@Type'] === 'string' && root['@Type'];
-
-                addTypeToResult(item as IncomingRoot, container);
-              }
-            }
-          } else if (typeof value === 'object' && value !== null && '@Type' in value) {
-            addTypeToResult(value as IncomingRoot, parent);
-          }
-        }
-
-        return result;
-      };
-
       // Convert the gpac structure to a tree format
-      const gpacTree = traverseGPAC(structure['IsoMediaFile']);
+      const gpacTree = traverseGPAC(structure['IsoMediaFile'], reportedUnknownBoxes);
       expect(gpacTree).toBeDefined();
 
-      const traverseMP4 = (root: Array<Box>): Tree => {
-        const result: Tree = {};
-
-        for (const box of root) {
-          if (box.type) {
-            let type = box.type;
-            if (type in result) {
-              const count = Object.keys(result).filter(key => key.startsWith(type)).length;
-              type = `${type}_${count}`;
-            }
-
-            if (testFile.includes('sg-tl-st') && type === 'strk_1') {
-              // FIXME: FFC issue (MPEGGroup/FileFormatConformance#155)
-              continue;
-            }
-
-            if (testFile.includes('video_2500000bps_0') && type === 'mdat_1') {
-              continue; // FIXME: FFC issue (MPEGGroup/FileFormatConformance#131)
-            }
-
-            if (box.boxes?.length > 0) {
-              result[type] = traverseMP4(box.boxes);
-            } else if (box['entries']) {
-              result[type] = traverseMP4(box['entries']);
-            } else if (box['item_infos']) {
-              result[type] = traverseMP4(box['item_infos']);
-            } else if (box['references']) {
-              result[type] = traverseMP4(box['references']);
-            } else {
-              // FIXME: FFC issue, maxr isn't present in the gpac structure (MPEGGroup/FileFormatConformance#157)
-              if (box.type === 'maxr') continue;
-
-              // FIXME: FFC issue, gpac de-duplicates entries in sample entries
-              // This isn't a proper fix, but FFC doesn't change that much anyway
-              if (type.startsWith('colr_')) continue;
-
-              result[type] = {};
-            }
-          }
-        }
-
-        return result;
-      };
-
       // Convert the MP4Box structure to a tree format
-      const mp4Tree = traverseMP4(mp4.boxes);
+      const mp4Tree = traverseMP4(mp4.boxes, testFile);
       expect(mp4Tree).toBeDefined();
 
       // Compare the structures
       expect(mp4Tree).toEqual(gpacTree);
+    });
+
+    test.for(
+      file_groups[groupName].filter(test => {
+        // FIXME: These files might be correct but I haven't investigated them yet
+        // Majority of the tests pass so I've left them out for now
+        const ignoredTests = [
+          'video_2500000bps_0.mp4',
+          'a3b-tone-deprot.mp4',
+          'a4-tone-fragmented.mp4',
+          'a6_tone_multifile.mp4',
+          'a7-tone-oddities.mp4',
+          'a9-aac-samplegroups-edit.mp4',
+          'hevc/hevc_tiles_multiple_tracks_empty_base.mp4',
+          'hevc/hevc_tiles_multiple_tracks.mp4',
+        ];
+        return !ignoredTests.includes(test);
+      }),
+    )('roundtrip %s', async testName => {
+      const { testFile } = getFilePath(groupName, testName);
+
+      // Get original file size
+      const originalSize = fs.statSync(testFile).size;
+
+      // Read the file
+      const { mp4 } = await loadAndGetInfo(testFile, true, true);
+      const originalTree = traverseMP4(mp4.boxes, testFile, true);
+
+      // Rewrite the file
+      const buffer = mp4.getBuffer();
+      buffer.seek(0); // Reset the stream position
+      expect(buffer.byteLength).toEqual(originalSize);
+
+      // Create a new MultiBufferStream with the buffer
+      const mp4Buffer = MP4BoxBuffer.fromArrayBuffer(buffer.buffer, 0);
+      const stream = new MultiBufferStream(mp4Buffer);
+
+      // Read the file again
+      const newMP4 = createFile(true, stream);
+      const newTree = traverseMP4(newMP4.boxes, testFile, true);
+
+      // Check the result
+      expect(newTree).toEqual(originalTree);
+
+      // Compare size
+      const newBuffer = newMP4.getBuffer();
+      expect(newBuffer.byteLength).toEqual(originalSize);
     });
   });
 });
