29cadbddc116a240f96a0e9048573d8545c521fd
Erwan Vivien <vivien.erwan@gmail.com>
2025-06-25 09:56:02 +0200

    chore: do not minify builds
    It's up to the user of the library to minify or not
    Also minifying means no one can apply patches[1] to the dist
    
    [1] https://pnpm.io/en/settings#patcheddependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -11,7 +11,7 @@ const iifeBuild = defineConfig({
   tsconfig: 'tsconfig.build.json',
   splitting: false,
   sourcemap: true,
-  minify: true,
+  minify: false,
   clean: true,
   dts: false,
 });
@@ -27,7 +27,7 @@ const regularBuild = defineConfig({
   tsconfig: 'tsconfig.build.json',
   splitting: false,
   sourcemap: true,
-  minify: true,
+  minify: false,
   clean: PUBLISH_TO_NPM,
   dts: true,
 });
