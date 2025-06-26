gpac/mp4box.js

commit f0a5dba29f54cfeb99151ba7b9ad3ab771a338b6

fix: type of onError and attach it to Log.error
closes #475

src/isofile.ts
@@ -178,9 +178,7 @@ export class ISOFile<TSegmentUser = unknown, TSampleUser = unknown> {
     | null = null;
   /** Callback to call when samples are ready */
   onSamples: ((id: number, user: TSampleUser, samples: Array<Sample>) => void) | null = null;
-  /** Callback to call when there is an error in the parsing or processing of samples */
-  onError: (() => void) | null = null;
-
+  /** Callback to call when an item is processed */
   onItem?: (() => void) | null = null;
   /** Boolean indicating if the moov box run-length encoded tables of sample information have been processed */
   sampleListBuilt = false;
@@ -232,6 +230,11 @@ export class ISOFile<TSegmentUser = unknown, TSampleUser = unknown> {
     } else {
       this.stream = new MultiBufferStream();
     }
+  }
+
+  /** Callback to call when there is an error in the parsing or processing of samples */
+  set onError(callback: Parameters<typeof Log.setErrorCallback>[0]) {
+    Log.setErrorCallback(callback);
   }
 
   setSegmentOptions(

src/log.ts
@@ -10,8 +10,12 @@ const LOG_LEVEL_INFO = 2;
 const LOG_LEVEL_DEBUG = 1;
 
 let log_level = LOG_LEVEL_ERROR;
+let errorCallback: ((module: string, msg?: string) => void) | undefined;
 
 export const Log = {
+  setErrorCallback(callback?: typeof errorCallback) {
+    errorCallback = callback;
+  },
   setLogLevel(level: (module: string, msg?: string) => void) {
     if (level === this.debug) log_level = LOG_LEVEL_DEBUG;
     else if (level === this.info) log_level = LOG_LEVEL_INFO;
@@ -53,7 +57,9 @@ export const Log = {
     }
   },
   error(module: string, msg?: string) {
-    if (LOG_LEVEL_ERROR >= log_level) {
+    if (errorCallback) {
+      errorCallback(module, msg);
+    } else if (LOG_LEVEL_ERROR >= log_level) {
       console.error(
         '[' + Log.getDurationString(new Date().getTime() - start.getTime(), 1000) + ']',
         '[' + module + ']',

commit 3a1df38f33babbfff767bf3daa18ef2d6672264b

fix: regression from f0a5dba29f54cfeb99151ba7b9ad3ab771a338b6

src/boxes/av1C.ts
@@ -25,12 +25,12 @@ export class av1CBox extends Box {
   parse(stream: MultiBufferStream) {
     let tmp = stream.readUint8();
     if (((tmp >> 7) & 0x1) !== 1) {
-      Log.error('av1C marker problem');
+      Log.error('BoxParser', 'av1C marker problem', stream.isofile);
       return;
     }
     this.version = tmp & 0x7f;
     if (this.version !== 1) {
-      Log.error('av1C version ' + this.version + ' not supported');
+      Log.error('BoxParser', 'av1C version ' + this.version + ' not supported', stream.isofile);
       return;
     }
     tmp = stream.readUint8();
@@ -47,7 +47,7 @@ export class av1CBox extends Box {
     tmp = stream.readUint8();
     this.reserved_1 = (tmp >> 5) & 0x7;
     if (this.reserved_1 !== 0) {
-      Log.error('av1C reserved_1 parsing problem');
+      Log.error('BoxParser', 'av1C reserved_1 parsing problem', stream.isofile);
       return;
     }
     this.initial_presentation_delay_present = (tmp >> 4) & 0x1;
@@ -56,7 +56,7 @@ export class av1CBox extends Box {
     } else {
       this.reserved_2 = tmp & 0xf;
       if (this.reserved_2 !== 0) {
-        Log.error('av1C reserved_2 parsing problem');
+        Log.error('BoxParser', 'av1C reserved_2 parsing problem', stream.isofile);
         return;
       }
     }

src/boxes/iinf.ts
@@ -31,7 +31,7 @@ export class iinfBox extends FullBox {
         if (box.type === 'infe') {
           this.item_infos[i] = box as infeBox;
         } else {
-          Log.error('BoxParser', "Expected 'infe' box, got " + ret.box.type);
+          Log.error('BoxParser', "Expected 'infe' box, got " + ret.box.type, stream.isofile);
         }
       } else {
         return;

src/boxes/stz2.ts
@@ -32,7 +32,7 @@ export class stz2Box extends FullBox {
           this.sample_sizes[i] = stream.readUint16();
         }
       } else {
-        Log.error('BoxParser', 'Error in length field in stz2 box');
+        Log.error('BoxParser', 'Error in length field in stz2 box', stream.isofile);
       }
     }
   }

src/buffer.ts
@@ -1,6 +1,7 @@
 import { DataStream, Endianness } from '#/DataStream';
 import { Log } from '#/log';
 import { MP4BoxBuffer } from '#/mp4boxbuffer';
+import type { ISOFile } from './isofile';
 
 /**
  * helper functions to concatenate two ArrayBuffer objects
@@ -27,6 +28,7 @@ function concatBuffers(buffer1: ArrayBuffer, buffer2: ArrayBuffer) {
  * It inherits also from DataStream for all read/write/alloc operations
  */
 export class MultiBufferStream extends DataStream {
+  isofile: ISOFile | null = null; // Reference to the ISOFile object if any
   buffers: Array<MP4BoxBuffer>;
   bufferIndex: number;
 

src/isofile.ts
@@ -180,6 +180,8 @@ export class ISOFile<TSegmentUser = unknown, TSampleUser = unknown> {
     | null = null;
   /** Callback to call when samples are ready */
   onSamples: ((id: number, user: TSampleUser, samples: Array<Sample>) => void) | null = null;
+  /** Callback to call when there is an error in the parsing or processing of samples */
+  onError: ((module: string, message: string) => void) | null = null;
   /** Callback to call when an item is processed */
   onItem?: (() => void) | null = null;
   /** Boolean indicating if the moov box run-length encoded tables of sample information have been processed */
@@ -232,11 +234,6 @@ export class ISOFile<TSegmentUser = unknown, TSampleUser = unknown> {
     } else {
       this.stream = new MultiBufferStream();
     }
-  }
-
-  /** Callback to call when there is an error in the parsing or processing of samples */
-  set onError(callback: Parameters<typeof Log.setErrorCallback>[0]) {
-    Log.setErrorCallback(callback);
   }
 
   setSegmentOptions(
@@ -401,6 +398,7 @@ export class ISOFile<TSegmentUser = unknown, TSampleUser = unknown> {
           Log.error(
             'ISOFile',
             `Invalid data found while parsing box of type '${ret.type}' at position ${ret.start}. Aborting parsing.`,
+            this,
           );
           break;
         }

src/log.ts
@@ -1,3 +1,5 @@
+import type { ISOFile } from './isofile';
+
 /*
  * Copyright (c) 2012-2013. Telecom ParisTech/TSI/MM/GPAC Cyril Concolato
  * License: BSD-3-Clause (see LICENSE file)
@@ -10,12 +12,8 @@ const LOG_LEVEL_INFO = 2;
 const LOG_LEVEL_DEBUG = 1;
 
 let log_level = LOG_LEVEL_ERROR;
-let errorCallback: ((module: string, msg?: string) => void) | undefined;
 
 export const Log = {
-  setErrorCallback(callback?: typeof errorCallback) {
-    errorCallback = callback;
-  },
   setLogLevel(level: (module: string, msg?: string) => void) {
     if (level === this.debug) log_level = LOG_LEVEL_DEBUG;
     else if (level === this.info) log_level = LOG_LEVEL_INFO;
@@ -56,9 +54,9 @@ export const Log = {
       );
     }
   },
-  error(module: string, msg?: string) {
-    if (errorCallback) {
-      errorCallback(module, msg);
+  error(module: string, msg?: string, isofile?: ISOFile) {
+    if (isofile?.onError) {
+      isofile.onError(module, msg);
     } else if (LOG_LEVEL_ERROR >= log_level) {
       console.error(
         '[' + Log.getDurationString(new Date().getTime() - start.getTime(), 1000) + ']',